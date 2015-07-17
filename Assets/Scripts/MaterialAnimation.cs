9cee8153c426db14915708418abb4bd8b1e4fe4a
JiangShu <jiangsyk@163.com>
2015-07-17 17:05:12 +0800

    animation

---
+++ Assets/Scripts/MaterialAnimation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Description: MaterialAnimation
+ * Author:      JiangShu
+ * Create Time: 2015/7/17 17:00:50
+ */
+[RequireComponent(typeof(MaterialManager))]
+public class MaterialAnimation : MonoBehaviour
+{
+    public int min;
+    public int max;
+    public int fps = 5;
+
+    float timer;
+    MaterialManager manager;
+    void Start()
+    {
+        manager = GetComponent<MaterialManager>();
+        manager.currentFrame = min;
+        manager.HandlerMaterial();
+        ResetTimer();
+    }
+    void Update()
+    {
+        if (manager == null) return;
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            ResetTimer();
+            manager.currentFrame++;
+            if(manager.currentFrame > max)
+            {
+                manager.currentFrame = min;
+            }
+            manager.HandlerMaterial();
+        }
+    }
+    void ResetTimer()
+    {
+        timer = 1f / fps;
+    }
+}
---
+++ Assets/Scripts/MaterialManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Description: MaterialManager
+ * Author:      JiangShu
+ * Create Time: 2015/7/17 16:50:19
+ */
+public class MaterialManager : MonoBehaviour
+{
+    public int columns = 8;//列 x num
+    public int rows = 8;//行 y num
+    public int currentFrame = 0;
+    void Start()
+    {
+        HandlerMaterial();
+    }
+    void Update()
+    {
+
+    }
+    [ContextMenu("Handle Material")]
+    public void HandlerMaterial()
+    {
+        Vector2 framePosition = new Vector2();
+        framePosition.x = currentFrame % columns;
+        framePosition.y = currentFrame / columns;
+        renderer.sharedMaterial.SetTextureScale("_MainTex", new Vector2(1f / columns, 1f / rows));
+        renderer.sharedMaterial.SetTextureOffset("_MainTex", new Vector2(framePosition.x / columns, 1 - (framePosition.y + 1) / rows));
+    }
+}
